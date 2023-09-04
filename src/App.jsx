1bcf6653ca284e74d9abacb7ee4fbb4229eb72de
CIAanalyst <143934595+CIAanalyst@users.noreply.github.com>
2023-09-04 21:04:24 +0500

    add all changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Talha Aqeel",
+  title: "Cybersecurity Enthusiast",
+  email: "talha.aqeel@studentambassadors.com",
+  gitHub: "TalhaAqeel",
+  instagram: "Talha",
+  linkedIn: "TalhaAqeel",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "TalhaAqeel",
+  youTube: "TDM",
 };
 
 const primaryColor = "#4E567E";
